7f2d5f4d4869f42e9b420b25b6b4906ec4505d7f
Kumodatsu <dragonkumo@gmail.com>
2022-08-15 14:29:32 +0200

    Move types into separate modules

---
+++ screentagger/src/data/query.rs
@@ -0,0 +1,128 @@
+use crate::data::tag::Tag;
+
+#[derive(Debug)]
+pub enum Query {
+  Tag(Tag),
+  Not(Box<Query>),
+  Or(Box<Query>, Box<Query>),
+  And(Box<Query>, Box<Query>),
+}
+
+pub fn satisfies(tag_list: &Vec<Tag>, query: Query) -> bool {
+  match query {
+    Query::Tag(tag)  => tag_list.contains(&tag),
+    Query::Not(a)    => !satisfies(tag_list, *a),
+    Query::Or(a, b)  => satisfies(tag_list, *a) || satisfies(tag_list, *b),
+    Query::And(a, b) => satisfies(tag_list, *a) && satisfies(tag_list, *b),
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::data::tag::literal;
+
+  #[test]
+  fn tag_list_includes_tag() {
+    let tag_list = vec![
+      literal("foo"),
+      literal("bar"),
+    ];
+
+    let result = satisfies(&tag_list, Query::Tag(literal("foo")));
+    assert_eq!(result, true);
+  }
+
+  #[test]
+  fn tag_list_does_not_include_tag() {
+    let tag_list = vec![
+      literal("foo"),
+      literal("bar"),
+    ];
+
+    let result = satisfies(&tag_list, Query::Tag(literal("bla")));
+    assert_eq!(result, false);
+  }
+
+  #[test]
+  fn tag_list_excludes_tag() {
+    let tag_list = vec![
+      literal("foo"),
+      literal("bar"),
+    ];
+
+    let result = satisfies(&tag_list, Query::Not(
+      Box::new(Query::Tag(literal("bla")))
+    ));
+    assert_eq!(result, true);
+  }
+
+  #[test]
+  fn tag_list_does_not_exclude_tag() {
+    let tag_list = vec![
+      literal("foo"),
+      literal("bar"),
+    ];
+
+    let result = satisfies(&tag_list, Query::Not(
+      Box::new(Query::Tag(literal("foo")))
+    ));
+    assert_eq!(result, false);
+  }
+
+  #[test]
+  fn tag_list_includes_either_tag() {
+    let tag_list = vec![
+      literal("foo"),
+      literal("bar"),
+    ];
+
+    let result = satisfies(&tag_list, Query::Or(
+      Box::new(Query::Tag(literal("foo"))),
+      Box::new(Query::Tag(literal("bla")))
+    ));
+    assert_eq!(result, true);
+  }
+
+  #[test]
+  fn tag_list_does_not_include_either_tag() {
+    let tag_list = vec![
+      literal("foo"),
+      literal("bar"),
+    ];
+
+    let result = satisfies(&tag_list, Query::Or(
+      Box::new(Query::Tag(literal("pow"))),
+      Box::new(Query::Tag(literal("bla")))
+    ));
+    assert_eq!(result, false);
+  }
+
+  #[test]
+  fn tag_list_includes_both_tags() {
+    let tag_list = vec![
+      literal("foo"),
+      literal("bar"),
+    ];
+
+    let result = satisfies(&tag_list, Query::And(
+      Box::new(Query::Tag(literal("foo"))),
+      Box::new(Query::Tag(literal("bar")))
+    ));
+    assert_eq!(result, true);
+  }
+
+  #[test]
+  fn tag_list_does_not_include_both_tags() {
+    let tag_list = vec![
+      literal("foo"),
+      literal("bar"),
+    ];
+
+    let result = satisfies(&tag_list, Query::And(
+      Box::new(Query::Tag(literal("foo"))),
+      Box::new(Query::Tag(literal("bla")))
+    ));
+    assert_eq!(result, false);
+  }
+}
---
+++ screentagger/src/data/tag.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Tag {
+  Literal(String),
+}
+
+pub fn literal(tag_name: &str) -> Tag {
+  Tag::Literal(String::from(tag_name))
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn literal_works() {
+    let a = literal("foo");
+    let b = Tag::Literal(String::from("foo"));
+    assert_eq!(a, b);
+  }
+}
