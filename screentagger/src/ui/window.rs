d1bee222f9a42305dc0bfbec906822b061c53ac2
Kumodatsu <dragonkumo@gmail.com>
2022-08-16 23:21:46 +0200

    Divide UI's HTML, CSS and Javascript into separate files

--- screentagger/src/ui/window.rs
+++ screentagger/src/ui/window.rs
@@ -3,7 +3,21 @@ use web_view::{Content};
 pub fn create_window() {
   web_view::builder()
     .title("Screentagger")
-    .content(Content::Html(include_str!("res/ui.html")))
+    .content(Content::Html(format!(
+      r#"
+      <!doctype html>
+      <html>
+        <head>
+          <style>{css}</style>
+        </head>
+        <body>{html}</body>
+        <script type="text/javascript">{js}</script>
+      </html>
+      "#,
+      css  = include_str!("res/style.css"),
+      html = include_str!("res/ui.html"),
+      js   = include_str!("res/script.js"),
+    )))
     .size(800, 600)
     .resizable(true)
     .debug(true)
