6d47aaa02456c6c97d99db0e157e6bcf61f9f412
Kumodatsu <dragonkumo@gmail.com>
2022-08-17 23:03:32 +0200

    Implement database functionality

--- screentagger/src/db/database.rs
+++ screentagger/src/db/database.rs
@@ -1,4 +1,5 @@
-use crate::data::tag::Tag;
+use crate::data::tag::{Tag, literal};
+use crate::data::query::{Query, satisfies};
 use std::collections::HashSet;
 use sqlite::State;
 
@@ -15,11 +16,11 @@ impl Database {
     db.connection.execute("
       CREATE TABLE IF NOT EXISTS file (
         id   INTEGER NOT NULL PRIMARY KEY,
-        name TEXT NOT NULL
+        name TEXT    NOT NULL UNIQUE
       );
       CREATE TABLE IF NOT EXISTS tag (
         id   INTEGER NOT NULL PRIMARY KEY,
-        name TEXT NOT NULL
+        name TEXT    NOT NULL UNIQUE
       );
       CREATE TABLE IF NOT EXISTS file_tag (
         file_id INTEGER NOT NULL,
@@ -52,7 +53,7 @@ impl Database {
       match tag {
         Tag::Literal(name) => {
           self.connection.execute(
-            format!("INSERT INTO tag (name) VALUES ('{}');", name)
+            format!("INSERT OR IGNORE INTO tag (name) VALUES ('{}');", name)
           ).expect("Failed to insert tag.");
         },
       }
@@ -62,9 +63,94 @@ impl Database {
   pub fn add_files(&self, files: &HashSet<String>) {
     for file in files {
       self.connection.execute(
-        format!("INSERT INTO file (name) VALUES ('{}');", file)
+        format!("INSERT OR IGNORE INTO file (name) VALUES ('{}');", file)
       ).expect("Failed to insert file.");
     }
+  }
+
+  pub fn add_tags_to_file(&self, file: String, tags: &HashSet<Tag>) {
+    let file_id = self.get_file_id(&file);
+    for tag in tags {
+      let tag_id = self.get_tag_id(tag);
+      self.connection
+        .execute(
+          format!("
+            INSERT OR IGNORE INTO file_tag (file_id, tag_id)
+            VALUES ({}, {});
+          ", file_id, tag_id)
+        )
+        .expect("Failed create file selection statement.");
+    }
+  }
+
+  pub fn get_tags_for_file(&self, file: &str) -> HashSet<Tag> {
+    self.connection
+      .prepare(
+        format!("
+          SELECT tag.name
+          FROM file
+            JOIN file_tag ON file.id = file_tag.file_id
+            JOIN tag      ON tag.id  = file_tag.tag_id
+          WHERE file.name = '{}'
+          ORDER BY tag.name ASC
+        ", file)
+      )
+      .expect("Failed create file selection statement.")
+      .into_cursor()
+      .filter_map(|res| {
+        if let Ok(row) = res {
+          Some(literal(&row.get::<String, _>(0)))
+        } else {
+          None
+        }
+      })
+      .collect::<HashSet<_>>()
+  }
+
+  pub fn query_files(&self, query: &Query) -> HashSet<String> {
+    self.connection.prepare("SELECT name FROM file")
+      .expect("Failed to create file selection statement.")
+      .into_cursor()
+      .filter_map(|res| {
+        if let Ok(row) = res {
+          let file_name = row.get::<String, _>(0);
+          let tags      = self.get_tags_for_file(&file_name);
+          if satisfies(&tags, &query) {
+            return Some(file_name);
+          }
+        }
+        None
+      })
+      .collect::<HashSet<_>>()
+  }
+
+  fn get_file_id(&self, file: &str) -> usize {
+    self.connection
+      .prepare(
+        format!("SELECT id FROM file WHERE name = '{}'", file)
+      )
+      .expect("Failed to create file selection statement.")
+      .into_cursor()
+      .next()
+      .expect("Queried file not present in database.")
+      .expect("Failed to get id from file.")
+      .get::<i64, _>(0)
+      as usize
+  }
+
+  fn get_tag_id(&self, tag: &Tag) -> usize {
+    let Tag::Literal(tag_name) = tag;
+    self.connection
+      .prepare(
+        format!("SELECT id FROM tag WHERE name = '{}'", tag_name)
+      )
+      .expect("Failed to create tag selection statement.")
+      .into_cursor()
+      .next()
+      .expect("Queried tag not present in database.")
+      .expect("Failed to get id from tag.")
+      .get::<i64, _>(0)
+      as usize
   }
 }
 
@@ -116,12 +202,106 @@ mod tests {
 
     db.add_files(&files);
     
-    let db_files = db.connection.prepare("SELECT name FROM file")
+    let db_files = db.connection
+      .prepare("SELECT name FROM file")
       .unwrap()
       .into_cursor()
       .map(|row| { row.unwrap().get::<String, _>(0) })
       .collect::<HashSet<_>>();
 
     assert_eq!(files, db_files);
+  }
+
+  #[test]
+  fn tags_can_be_added_to_files() {
+    let db = Database::new(":memory:");
+    let files = HashSet::from([
+      String::from("foo.jpg"),
+      String::from("bar.jpg"),
+      String::from("bla.jpg"),
+    ]);
+    let tags = HashSet::from([
+      literal("foo"),
+      literal("bar"),
+      literal("bla"),
+    ]);
+
+    db.add_files(&files);
+    db.add_tags(&tags);
+
+    db.add_tags_to_file(
+      String::from("foo.jpg"),
+      &HashSet::from([
+        literal("foo"),
+        literal("bla"),
+      ]),
+    );
+
+    let db_assoc_count = db.connection
+      .prepare("SELECT file_id, tag_id FROM file_tag")
+      .unwrap()
+      .into_cursor()
+      .count();
+    
+    assert_eq!(db_assoc_count, 2);
+  }
+
+  #[test]
+  fn tags_can_be_retrieved_for_files() {
+    let db = Database::new(":memory:");
+    let files = HashSet::from([
+      String::from("foo.jpg"),
+      String::from("bar.jpg"),
+      String::from("bla.jpg"),
+    ]);
+    let tags = HashSet::from([
+      literal("foo"),
+      literal("bar"),
+      literal("bla"),
+    ]);
+
+    db.add_files(&files);
+    db.add_tags(&tags);
+
+    let foo_tags = HashSet::from([
+      literal("foo"),
+      literal("bla"),
+    ]);
+
+    db.add_tags_to_file(
+      String::from("foo.jpg"),
+      &foo_tags,
+    );
+
+    let tags = db.get_tags_for_file("foo.jpg");
+    assert_eq!(tags, foo_tags);
+  }
+
+  #[test]
+  fn files_can_be_queried() {
+    let db = Database::new(":memory:");
+    let files = HashSet::from([
+      String::from("foo.jpg"),
+      String::from("bar.jpg"),
+    ]);
+    let tags = HashSet::from([
+      literal("foo"),
+      literal("bla"),
+    ]);
+
+    db.add_files(&files);
+    db.add_tags(&tags);
+
+    db.add_tags_to_file(
+      String::from("foo.jpg"),
+      &HashSet::from([
+        literal("foo"),
+        literal("bla"),
+      ]),
+    );
+
+    let expected     = HashSet::from([String::from("foo.jpg")]);
+    let query_result = db.query_files(&Query::Tag(literal("foo")));
+    assert_eq!(query_result, expected);
   }
 }
