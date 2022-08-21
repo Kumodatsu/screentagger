cf14ae54e416a31a8a7c9b64a0317560e1bb48a6
Kumodatsu <dragonkumo@gmail.com>
2022-08-21 23:51:46 +0200

    Remove temporary database values

--- screentagger/src/main.rs
+++ screentagger/src/main.rs
@@ -5,25 +5,6 @@ mod file;
 
 fn main() {
   let mut app = app::app::App::new();
-  let db      = app.get_database();
-  db.add_files(&HashSet::from([
-    String::from("rawr.jpg"),
-    String::from("meow.jpg"),
-  ]));
-  db.add_tags(&HashSet::from([
-    literal("image"),
-    literal("dragon"),
-    literal("cat"),
-  ]));
-  db.add_tags_to_file(String::from("rawr.jpg"), &HashSet::from([
-    literal("image"),
-    literal("dragon"),
-  ]));
-  db.add_tags_to_file(String::from("meow.jpg"), &HashSet::from([
-    literal("image"),
-    literal("cat"),
-  ]));
-
   app::window::create_window(&mut app);
 }
 
