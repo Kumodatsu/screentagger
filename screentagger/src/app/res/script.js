c288dbfc9fe6176d996a6342a6a56ec0bf4c4e91
Kumodatsu <dragonkumo@gmail.com>
2022-08-21 23:58:16 +0200

    Display query matches in UI

--- screentagger/src/app/res/script.js
+++ screentagger/src/app/res/script.js
@@ -12,14 +12,31 @@ let addFolder = function() {
   invokeCommand({ command: "addFolder" });
 }
 
+let revealFile = function(filePath) {
+  invokeCommand({ command: "revealFile", filePath: filePath });
+}
+
+let makeThumbnail = function(file) {
+  let thumbnail = document.createElement("div");
+  thumbnail.setAttribute("class", "thumbnail");
+  let img = document.createElement("img");
+  img.setAttribute("src", file);
+  img.onclick = function() { revealFile(file); }
+  let p = document.createElement("p");
+  p.innerText = file;
+
+  thumbnail.appendChild(img).appendChild(p);
+
+  return thumbnail;
+}
+
 let displayMatches = function(matches) {
-  let ul = document.getElementById("file-display-list");
-  while (ul.firstChild) {
-    ul.removeChild(ul.lastChild);
+  let container = document.getElementById("file-display-container");
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
   }
   matches.forEach(function (match) {
-    let li = document.createElement("li");
-    li.innerHTML = match;
-    ul.appendChild(li);
+    let thumbnail = makeThumbnail(match);
+    container.appendChild(thumbnail);
   });
 }
