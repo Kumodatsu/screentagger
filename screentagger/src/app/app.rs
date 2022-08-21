598577b7b46ca39204911469d4482bba9d1e156a
Kumodatsu <dragonkumo@gmail.com>
2022-08-21 23:53:09 +0200

    Add add_folder function to App

--- screentagger/src/app/app.rs
+++ screentagger/src/app/app.rs
@@ -1,6 +1,9 @@
 use crate::data::parse::parse_query;
+use crate::data::tag::literal;
 use crate::db::database::Database;
 use std::collections::HashSet;
+use std::fs;
+use std::path::Path;
 
 pub struct App {
   db: Database,
@@ -8,9 +11,11 @@ pub struct App {
 
 impl App {
   pub fn new() -> Self {
-    App {
+    let app = App {
       db: Database::new(":memory:"),
-    }
+    };
+    app.db.add_tags(&HashSet::from([literal("image")]));
+    app
   }
 
   pub fn get_database(&mut self) -> &Database {
@@ -22,6 +27,21 @@ impl App {
       self.db.query_files(&query)
     } else {
       HashSet::from([])
+    }
+  }
+
+  pub fn add_folder<P: AsRef<Path>>(&self, path: P) {
+    let files = fs::read_dir(path)
+      .expect("Invalid folder.")
+      .filter_map(|entry| if let Ok(entry) = entry {
+        Some(String::from(entry.path().to_str().expect("Invalid path.")))
+      } else {
+        None 
+      })
+      .collect::<HashSet<_>>();
+    self.db.add_files(&files);
+    for file in files {
+      self.db.add_tags_to_file(file, &HashSet::from([literal("image")]));
     }
   }
 }
